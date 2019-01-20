4e1156c197dc13bbf79d7a79266a452dacf7a022
DESKTOP-DVRJQRM\Rajin <rajin>
2019-01-20 15:16:01 -0500

    Workin

---
+++ Scripts/s_level_generator.gd
@@ -0,0 +1,15 @@
+extends Node
+
+export var path_length_min : float = 30
+export var path_length_max : float = 50
+
+export var path_width_min : float = 5
+export var path_width_max : float = 10
+
+export var path_range_x : float = 30
+
+func _process(delta):
+	
+	pass
+	
+func 
